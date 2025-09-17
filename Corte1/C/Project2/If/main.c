3aef1a8b99de789a3bf0e16b0f1e7d9dadcf4518
SAbello08 <sebastianabello08@outlook.com>
2025-09-17 09:46:26 -0500

    Add files via upload

---
+++ Corte1/C/Project2/If/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(){
+	int number = 10;
+	if (number > 5){
+		printf("The number is greater than 5.\n");
+	}
+	return 0;
+}
